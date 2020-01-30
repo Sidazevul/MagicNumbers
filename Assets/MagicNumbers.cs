422438193eb0da650675d3608c72394d5dd7b858
Sidazevul <60444258+Sidazevul@users.noreply.github.com>
2020-01-30 19:15:27 +0300

    Initial commit

---
+++ Assets/MagicNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicNumbers : MonoBehaviour
+{
+    int minNumbers = 1;
+    int maxNumbers = 1000;
+    int result;
+
+    
+    void Start()
+    {
+        Debug.Log("Добро пожаловать в Magic Numbers");
+        Debug.Log("Загадайте ваше число");
+        Debug.Log("Минимальное число: " + minNumbers);
+        Debug.Log("Максмальное число: " + maxNumbers);
+        result = minNumbers * maxNumbers / 2;
+        Debug.Log("Твое число: " + result + " ?");
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && result < 1000)
+        {
+            result = result + 250;
+            Debug.Log("Может твое число: " + result + " ?");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow)  && result > 1)
+        {
+            result = result - 250;
+            Debug.Log("Может твое число: " + result + " ?");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return) || (Input.GetKeyDown(KeyCode.KeypadEnter)))
+        {
+            Debug.Log("Изи, твое число: " + result);
+        }
+        
+        
+    }
+}
