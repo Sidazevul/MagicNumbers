0a62e8286ad888bc4e03db03d105e88e886d828f
Sidazevul <60444258+Sidazevul@users.noreply.github.com>
2020-02-04 21:17:53 +0300

    Adds links on script

---
+++ Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Player PlayerVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(PlayerVar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
